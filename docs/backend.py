4bf0eab70f22c038c57b311e5fc627f1de1cbef1
JuanPombo <juan.pombo@correounivalle.edu.co>
2025-04-19 23:57:07 -0500

    cambio backend

--- docs/backend.py
+++ docs/backend.py
@@ -44,4 +44,5 @@ def update_sheet():
         return jsonify({"success": False, "message": f"Error del servidor: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0")
+
